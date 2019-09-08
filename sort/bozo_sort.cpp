c593b0336a4edd5365a338e404b12670e5f9ee77
shunail2029 <tiger1998nov27@gmail.com>
2019-09-08 19:57:30 +0900

    add bozo_sort

---
+++ sort/bozo_sort.cpp
@@ -0,0 +1,54 @@
+#include <random>
+#include <utility>
+
+template<class RandomAccessIterater>
+bool sorted(RandomAccessIterater first, RandomAccessIterater last) {
+    int len = last - first;
+    for (int i=0; i<len-1; i++) {
+        if (*(first+i) > *(first+i+1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <class RandomAccessIterater, class Compare>
+bool sorted(RandomAccessIterater first, RandomAccessIterater last, Compare cmp) {
+    int len = last - first;
+    for (int i=0; i<len-1; i++) {
+        if (cmp(*(first+i+1), *(first+i))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<class RandomAccessIterater>
+void bozo_sort(RandomAccessIterater first, RandomAccessIterater last) {
+    std::random_device seed_gen;
+    std::default_random_engine engine(seed_gen());
+    
+    int len = last - first;
+
+    std::uniform_int_distribution<> dist(0, len-1);
+
+    while (!::sorted(first, last)) {
+        int i = dist(engine), j = dist(engine);
+        std::swap(*(first+i), *(first+j));
+    }
+}
+
+template<class RandomAccessIterater, class Compare>
+void bozo_sort(RandomAccessIterater first, RandomAccessIterater last, Compare cmp) {
+    std::random_device seed_gen;
+    std::default_random_engine engine(seed_gen());
+
+    int len = last - first;
+
+    std::uniform_int_distribution<> dist(0, len-1);
+
+    while (!::sorted(first, last, cmp)) {
+        int i = dist(engine), j = dist(engine);
+        std::swap(*(first+i), *(first+j));
+    }
+}
